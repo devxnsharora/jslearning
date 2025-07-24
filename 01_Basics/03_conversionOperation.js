0589514fad149e68332543f08413b79c67d3395f
devxnsharora <aroradevansh26@gmail.com>
2025-07-24 11:35:35 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+let score2 = "33"
+console.log(typeof score)
+console.log(typeof(score2))
+
+let valueInNumber = Number(score2)
+
+ console.log(valueInNumber)// if scorre2 is 33A then it will give NaN
+
+console.log(typeof(valueInNumber))
+
+//COnversion to number
+// "33" => 33
+// "33abc" => NaN
+// true=>1; false=>0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//Conversion to boolean
+//1 => true ; 0=> false
+//"" =>false
+//"devansh" => true
+
+//STRING CONVERSIONS
+let someNumber =33
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
